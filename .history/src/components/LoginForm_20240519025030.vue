8d74b030b83a80d886210d0b5d185f2ee2478b2f
Sercan ISLI <sercanislii@gmail.com>
2023-05-18 12:00:00 +0300

    Login form implemented to Welcome component

---
+++ .history/src/components/LoginForm_20240519025030.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="E- mail" v-model="email" />
+    <input type="password" required placeholder="Password" v-model="password" />
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {
+      console.log(email.value, password.value);
+    };
+
+    return { displayName, email, password };
+  },
+};
+</script>
+
+<style></style>
---
+++ .history/src/views/Welcome_20240519024942.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+    <SignupForm />
+  </div>
+</template>
+
+<script>
+import SignupForm from "../components/SignupForm.vue";
+import LoginForm from "../components/LoginForm.vue";
+
+export default {
+  components: {
+    SignupForm,
+    LoginForm,
+  },
+};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
