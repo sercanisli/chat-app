fda05e84e2381c27b28e7a945d0b255ad15dbcf8
Sercan ISLI <sercanislii@gmail.com>
2024-05-15 23:32:21 +0300

    firebase added to project and firebase config added.

---
+++ .history/src/firebase/config_20240515232910.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDg1yRKponWWdQNSHhwyZXJQg_rEy60AzU",
+  authDomain: "chat-app-51944.firebaseapp.com",
+  projectId: "chat-app-51944",
+  storageBucket: "chat-app-51944.appspot.com",
+  messagingSenderId: "867799189161",
+  appId: "1:867799189161:web:0455b302973654d1c08bb8"
+};
