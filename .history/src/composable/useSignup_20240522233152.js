168da4c98633057f20e5ed719d545a4ffe8a5690
Sercan ISLI <sercanislii@gmail.com>
2024-05-22 23:32:34 +0300

    Refactoring

---
+++ .history/src/composable/useSignup_20240522233152.js
@@ -0,0 +1,27 @@
+import { ref } from 'vue';
+import { projectAuth } from "../firebase/config";
+
+const error = ref(null);
+const signup = async (email, password, displayName) => {
+    error.value = null
+    try {
+        const response = await projectAuth.createUserWithEmailAndPassword(email, password)
+        if (!response) {
+            throw new Error("Could not complete the signup!")
+        }
+        await response.user.updateProfile({ displayName });
+        error.value = null;
+        return response;
+        console.log(response.user)
+    } catch (err) {
+        error.value = err.message;  
+    }
+}
+
+
+const useSignup = () => {
+    return {error, signup}
+}
+
+
+export default useSignup;
