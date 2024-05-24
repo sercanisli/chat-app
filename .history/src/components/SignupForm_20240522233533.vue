6d629422ca1baa590f80d6d51a6f607815ddeaa9
Sercan ISLI <sercanislii@gmail.com>
2024-05-24 15:47:42 +0300

    useLogin composable created

---
+++ .history/src/components/SignupForm_20240522233533.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="handleSubmit()">
+    <input
+      type="text"
+      required
+      placeholder="Display Name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="E- mail" v-model="email" />
+    <input type="password" required placeholder="Password" v-model="password" />
+    <div class="error">{{ error }}</div>
+    <button type="submit">Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+import useSignup from "./../composable/useSignup.js";
+export default {
+  setup() {
+    const { error, signup } = useSignup();
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      console.log(displayName.value, email.value, password.value);
+      await signup(email.value, password.value, displayName.value);
+    };
+
+    return { displayName, email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style></style>
---
+++ .history/src/composable/useLogin_20240524154740.js
@@ -0,0 +1,18 @@
+import { ref } from 'vue';
+import { projectAuth } from "../firebase/config";
+
+const error = ref(null);
+
+const login = async (email, password) => {
+    error.value = null;
+
+    try {
+        const response = await projectAuth.signInWithEmailAndPassword(email, password);
+        error.value = null;
+        console.log(response);
+        return response;
+    } catch (err) {
+        console.log(err.value);
+        error.value = 'Incorrect email or password';
+    }
+}
